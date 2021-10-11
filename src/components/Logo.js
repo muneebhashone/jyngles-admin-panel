2c955552cbeac848f72a673d71057e445360db5c
muneebhashone <muneeb.hashone@gmail.com>
2021-10-11 14:28:10 +0500

    Logo fix

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,5 +1,5 @@
-const Logo = (props) => (
-  <img alt="Logo" src="static/jyngles-logo-mini.png" {...props} />
-);
+import LogoImg from '../assets/jyngles-logo-mini.png';
+
+const Logo = (props) => <img alt="Logo" src={LogoImg} {...props} />;
 
 export default Logo;
